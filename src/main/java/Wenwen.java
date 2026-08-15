ac53bea46b4ea1de12ba079e14b6df3d69810daf
wangqichen <wangqichen517@gmail.com>
2026-08-15 10:34:03 +0800

    name changing from Duke to Wenwen

---
+++ src/main/java/Wenwen.java
@@ -0,0 +1,18 @@
+public class Wenwen {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = " __        __                                  \n"
+          + " \\ \\      / /__ _ ____      _____ _ __       \n"
+          + "  \\ \\ /\\ / / _ \\ '_ \\ \\ /\\ / / _ \\ '_ \\      \n"
+          + "   \\ V  V /  __/ | | \\ V  V /  __/ | | |     \n"
+          + "    \\_/\\_/ \\___|_| |_|\\_/\\_/ \\___|_| |_|     \n";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Wenwen.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
