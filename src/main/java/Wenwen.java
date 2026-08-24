2c3ccdd2a974abda6d1b609327a67a74268a9561
wangqichen <wangqichen517@gmail.com>
2026-08-24 22:03:10 +0800

    Add Level-2 task list support

--- src/main/java/Wenwen.java
+++ src/main/java/Wenwen.java
@@ -14,17 +14,27 @@ public class Wenwen {
         System.out.println("What can I do for you?");
         System.out.println(line);
 	Scanner scanner = new Scanner(System.in);
+    String[] task = new String [100];
+    int taskCount = 0;
 	while(true){
         String input = scanner.nextLine();
-	System.out.println(line);
-	if(input.equals("bye"))
-	{
-        	System.out.println("Bye. Hope to see you again soon!");
-		System.out.println(line);
-		break;
-	}
-	System.out.println(input);
-        System.out.println(line);
+	    System.out.println(line);
+	    if(input.equals("bye"))
+	    {
+            System.out.println("Bye. Hope to see you again soon!");
+		    System.out.println(line);
+		    break;
+	    }else if(input.equals("list")){
+            for(int i = 0; i < taskCount; i++){
+                System.out.println((i+1) + ". "+task[i]);
+            }
+            System.out.println(line);
+        }else{
+            task[taskCount] = input;
+            taskCount++;
+	        System.out.println("added: "+input);
+            System.out.println(line);
+        }
 	}
     }
 }
