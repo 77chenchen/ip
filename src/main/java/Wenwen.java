f5e696e7ccd13d6390f3a0c16004ce3dc0ec18c5
wangqichen <wangqichen517@gmail.com>
2026-08-24 21:37:34 +0800

    echo

--- src/main/java/Wenwen.java
+++ src/main/java/Wenwen.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Wenwen {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -6,13 +7,24 @@ public class Wenwen {
           + "  \\ \\ /\\ / / _ \\ '_ \\ \\ /\\ / / _ \\ '_ \\      \n"
           + "   \\ V  V /  __/ | | \\ V  V /  __/ | | |     \n"
           + "    \\_/\\_/ \\___|_| |_|\\_/\\_/ \\___|_| |_|     \n";
-
+	
         System.out.println(line);
         System.out.println(banner);
         System.out.println("Hello! I'm Wenwen.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+	Scanner scanner = new Scanner(System.in);
+	while(true){
+        String input = scanner.nextLine();
+	System.out.println(line);
+	if(input.equals("bye"))
+	{
+        	System.out.println("Bye. Hope to see you again soon!");
+		System.out.println(line);
+		break;
+	}
+	System.out.println(input);
         System.out.println(line);
+	}
     }
 }
